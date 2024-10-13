60137fab583de158c200f163c35450bbd492f677
Darya Jenkins <melissegreenleaf@gmail.com>
2024-10-13 23:23:38 +0200

    Fix

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate(double m, int kg) {
+    public int calculate(double m, int kg) {
         double bmi;
         bmi = kg / (m*m);
-        return bmi;
+        return (int) bmi;
     }
 }
